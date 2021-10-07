18afd2cb247f6acfec4b8c01bf6a1e60eb03609b
배수현 <suethebae@gmail.com>
2021-10-07 19:02:42 +0900

    Querydsl 하다가 대가리 깨지는중2

--- src/main/java/org/board/test/web/IndexController.java
+++ src/main/java/org/board/test/web/IndexController.java
@@ -1,13 +1,20 @@
 package org.board.test.web;
 
+import lombok.RequiredArgsConstructor;
+import org.board.test.service.posts.PostsService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
